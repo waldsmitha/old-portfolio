4a4b78bf0d064d8ea49de7fe746ac95f4e0ef933
Austin Waldsmith <austin.waldsmith@gmail.com>
2021-03-20 20:37:58 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const listItems =   
+    document.querySelectorAll("li");
+    photos = document.querySelectorAll(".project");
+
+listItems.forEach((li, i) => {
+  li.addEventListener("mouseenter", () => {
+    photos[i].classList.add("intro");
+  });
+  li.addEventListener("mouseleave", () => {
+    photos[i].classList.remove("intro");
+  });
+
+
+  li.addEventListener("mousemove", (e) => {
+    console.log(e)
+    photos[i].style.left = - e.movementX * 1.5 + 'px';
+    photos[i].style.top = - e.clientY * .5 + 'px';
+  });
+});
+
+console.log(window.innerWidth)
